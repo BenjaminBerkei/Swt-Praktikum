ccf08992cb921ed43da952711b46dcb069fb2229
DgeneratedMonkey <32734182+DgeneratedMonkey@users.noreply.github.com>
2018-01-12 11:40:40 +0100

    Basic

--- dynamicunit.hpp
+++ dynamicunit.hpp
@@ -1,9 +1,9 @@
 /////////////////////////////////////////////
-// staticunit.hpp // letzte Änderung: 16.12.17 //
+// dynamicunit.hpp // letzte Änderung: 10.1.17 //
 // Letzte Editirung: Arne				   //
-// Version: 0.1							   //
+// Version: 0.2						   //
 // -------- Kommentare --------------------//
-/*
+/* Wer noch irgendwo Static statt Dynamic findet bekommt n Eis von mir, lg Arne
 */
 /////////////////////////////////////////////
 
@@ -17,139 +17,180 @@
 class DynamicUnit : public Unit
 {
 protected:
-	int int_unitAutoRep;
+  int int_unitAutoRep;
   int int_unitMoveRange;
   int int_unitEXP;
   int int_unitAirAtt;
-  int int_unitAirRange;
+  //int int_unitAirRange;
   int int_unitGroundAtt;
-  int int_unitGroundRange;
+  //int int_unitGroundRange;
   int int_unitWaterAtt;
-  int int_unitWaterRange;
+  //int int_unitWaterRange;
   int int_unitLevel;
 
 public:
-	StaticUnit(string, Player*, int, int, int, string, int, bool, int);
-	~StaticUnit();
+DynamicUnit(QString, Player*);
+~DynamicUnit();
 
-	int getUnitAutoRep() const;
+  int getUnitAutoRep() const;
   int getUnitMoveRange() const;
   int getUnitEXP() const;
   int getUnitAirAtt() const;
-  int getUnitAirRange() const;
+  //int getUnitAirRange() const;
   int getUnitGroundAtt() const;
-  int getUnitGroundRange() const;
+  //int getUnitGroundRange() const;
   int getUnitWaterAtt() const;
-  int getUnitWaterRange() const;
+  //int getUnitWaterRange() const;
   int getUnitLevel() const;
 
   void setUnitAutoRep(const int);
   void setUnitMoveRange(const int);
   void setUnitEXP(const int);
   void setUnitAirAtt(const int);
-  void setUnitAirRange(const int);
+  //void setUnitAirRange(const int);
   void setUnitGroundAtt(const int);
-  void setUnitGroundRange(const int);
+  //void setUnitGroundRange(const int);
   void setUnitWaterAtt(const int);
-  void setUnitWaterRange(const int);
+  //void setUnitWaterRange(const int);
   void setUnitLevel(const int);
+  
+  void autoRepair();
+  void levelUpBonus();
+
 };
+
+
 
 // Air
 
-class AirUnit : public StaticUnit
+class AirUnit : public DynamicUnit
 {
 public:
-	AirUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	AirUnit(QString, Player*);
 	~AirUnit();
+	
+	int moveTo(HexagonMatchfield* );
+	bool action(HexagonMatchfield* );
 };
 
-class TransporterAirUnit : public AirUnit
+// Transporter
+
+class TransporterUnit : public DynamicUnit
 {
-private:
+protected:
 	int int_transporterUnitCapacity;
 	int int_transporterUnitCurrentCapacity;
 
 public:
-	TransporterAirUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int, int, int);
-	~TransporterAirUnit();
+	TransporterUnit(QString, Player*);
+	~TransporterUnit();
 	int getTransporterUnitCapacity() const;
-  int getTransporterUnitCurrentCapacity() const;
-  void setTransporterUnitCurrentCapacity(const int);
+  	int getTransporterUnitCurrentCapacity() const;
+  	void setTransporterUnitCurrentCapacity(const int);
+};
+
+//TransporterAir
+
+class TransporterAirUnit : public TransporterUnit
+{
+public:
+	TransporterAirUnit(QString, Player*);
+	~TransporterAirUnit();
+  
+  int moveTo(HexagonMatchfield* );
+  //bool action(HexagonMatchfield* );
+};
+
+//TransporterGround
+
+class TransporterGroundUnit : public TransporterUnit
+{
+public:
+	TransporterGroundUnit(QString, Player*);
+	~TransporterGroundUnit();
+
+  int moveTo(HexagonMatchfield* );
+  //bool action(HexagonMatchfield* );
+};
+
+// TransporterWater
+
+class TransporterWaterUnit : public TransporterUnit
+{
+public:
+	TransporterWaterUnit(QString, Player*);
+	~TransporterWaterUnit();
+  
+  int moveTo(HexagonMatchfield* );
+  //bool action(HexagonMatchfield* );
 };
 
 //Ground
 
-class GroundUnit : public StaticUnit
+class GroundUnit : public DynamicUnit
 {
 public:
-	GroundUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	GroundUnit(QString, Player*);
 	~GroundUnit();
+	
+	 bool action(HexagonMatchfield* );
 };
+
+// Light
 
 class LightUnit : public GroundUnit
 {
 public:
-	LightUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	LightUnit(QString, Player*);
 	~LightUnit();
+	
+	int moveTo(HexagonMatchfield* );
 };
+
+// BuilderLight
 
 class BuildLightUnit : public LightUnit
 {
 public:
-	BuildLightUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	BuildLightUnit(QString, Player*);
 	~BuildLightUnit();
+	
+	bool action(HexagonMatchfield* );
 };
+
+// Medium
 
 class MediumUnit : public GroundUnit
 {
 public:
-	MediumUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	MediumUnit(QString, Player*);
 	~MediumUnit();
+	
+	int moveTo(HexagonMatchfield* );
 };
+
+// Heavy
 
 class HeavyUnit : public GroundUnit
 {
 public:
-	HeavyUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	HeavyUnit(QString, Player*);
 	~HeavyUnit();
+	
+	int moveTo(HexagonMatchfield* );
 };
 
-class TransporterGroundUnit : public GroundUnit
-{
-private:
-	int int_transporterUnitCapacity;
-	int int_transporterUnitCurrentCapacity;
-
-public:
-	TransporterGroundUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int, int, int);
-	~TransporterGroundUnit();
-	int getTransporterUnitCapacity() const;
-  int getTransporterUnitCurrentCapacity() const;
-  void setTransporterUnitCurrentCapacity(const int);
-};
 
 // Water
 
-class WaterUnit : public StaticUnit
+class WaterUnit : public DynamicUnit
 {
 public:
-	WaterUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int);
+	WaterUnit(QString, Player*);
 	~WaterUnit();
-};
-
-class TransporterWaterUnit : public WaterUnit
-{
-private:
-	int int_transporterUnitCapacity;
-	int int_transporterUnitCurrentCapacity;
-
-public:
-	TransporterWaterUnit((string, Player*, int view, int, string, int, bool, int, int, int, int, int, int, int, int, int, int);
-	~TransporterWaterUnit();
-	int getTransporterUnitCapacity() const;
-  int getTransporterUnitCurrentCapacity() const;
-  void setTransporterUnitCurrentCapacity(const int);
+	
+	int moveTo(HexagonMatchfield* );
+	bool action(HexagonMatchfield* );
 };
 
 #endif
